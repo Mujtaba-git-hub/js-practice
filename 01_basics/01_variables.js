0356d716626744f3076bebc06b8c609085a2f9d6
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-20 13:02:34 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,22 @@
 const AccountId = 786;  // ; in end is optional , Const will nevere change its value throug out programe
 
-let AccountEmail = "Mujtaba@gmail.com"
+let AccountEmail = "Mujtaba@gmail.com" 
 
 var AccountPassword = "Mpk"
 
-AccountId= 3
+AccountCity = "karachi"
+// AccountId= 3 NOT ALLOWED     erro bcz const never chnages 
+
 
 console.log(AccountId)
+
+AccountEmail= "abc@gmail.com"
+AccountPassword= "2121"
+AccountCity = "NewYork"
+
+console.table([AccountId, AccountEmail, AccountCity, AccountPassword])
+
+/* 
+ --> PRefer not to use var , bcz of block and functional scope
+ --> by default variable value is Undefiend 
+*/
