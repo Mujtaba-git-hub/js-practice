d6e056c788d2d4e0c4e8d5792d8383f9cb77d10f
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-20 12:50:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const AccountId = 786;  // ; in end is optional , Const will nevere change its value throug out programe
+
+let AccountEmail = "Mujtaba@gmail.com"
+
+var AccountPassword = "Mpk"
+
+AccountId= 3
+
+console.log(AccountId)
