9fa6a9518400faf32d8776f578a3feadf1de7baa
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-21 12:26:34 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,3 +3,21 @@
 // alert( 3 + 3 ) we are using nodejs ,not browser
 
 console.log("data types");
+
+let name = "Mujtaba"
+let age = 18    
+let isloggedIN = false
+
+// number range =>2 to power 52 or 53
+// bigint 
+// string => ""
+// boolena = false/true
+// null => standalone value , null is reprenstation of empty value
+// undefine => means value not assigned
+// symbol => unique
+
+// object
+
+console.table([typeof(null), typeof(undefined), typeof (name), typeof(age)])
+
+// console.table([typeof(null), typeof(undefined), typeof(name), typeof(age)]);
