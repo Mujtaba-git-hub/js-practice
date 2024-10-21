30f244d7912df5fd0371013a1d404069152cfb71
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-21 13:06:13 +0000

    typeConvertion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,51 @@
-let score = 57
+let score = "33abc"
 
 console.log(typeof score)
 console.log(typeof(score))
 
+let valueInNumber = Number(score) // datatype write in First capital
+console.log( "\n",typeof(valueInNumber))
+
+/*
+ convertion into Number
+ Null => 0
+ undefine => NaN   , but its type is Number
+ "Mujtaba" => NaN  , but its type is Number
+ "34" => 34 
+ true => 1 ; false => 0
+ 
+ */
+
+ let A = Boolean("")
+ let B = Boolean("abc")
+ let C = Boolean("1")
+ let D = Boolean("0")
+ let E = Boolean(null)
+ let F = Boolean(undefined)
+
+//  console.table([
+//     { Variable: "emptystring", Value: A, Type: typeof(A) },
+//     { Variable: "abc", Value: B, Type: typeof(B) },
+//     { Variable: "1", Value: C, Type: typeof(C) },
+//     { Variable: "0", Value: D, Type: typeof(D) },
+//     { Variable: "null", Value: E, Type: typeof(E) },
+//     { Variable: "undefiend", Value: F, Type: typeof(F) }
+// ]);
+
+/* converting into bool
+ 1 => true
+ 0 => flase
+ "Mujtaba" => ture
+ undefiend => false
+ null => false
+*/
+
+let g = String(1)
+let h = String(undefined)
+let i = String(null)
+
+console.table[
+    {variable:"g", value:g, type: typeof(g)},
+    {variable:"g", value:g, type: typeof(h)},
+    {variable:"g", value:g, type: typeof(i)}
+]
