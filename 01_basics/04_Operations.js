76a3177a0f98db2bf6053f3212af19b93d060c77
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-23 12:44:07 +0000

    operations

--- 01_basics/04_Operations.js
+++ 01_basics/04_Operations.js
@@ -1,4 +1,11 @@
 let value = 3
 let negValue= -value
 
-console.log(value)
+console.log(negValue)
+
+ console.log("1" + 0)  //  => 10  , first string then treat all string
+ console.log(1 + "2")   // => 12  ,if string last then first other conversion solve then string solve
+ console.log("1" + 2 + 2)//=> 122 ,if string  first then all treat as a string
+ console.log(1 + 2 + "2")//=> 32  ,if string last then first other conversion solve then string solve
+ 
+01
