572c65024dc230d4c6ed50e8732b9b730429c9c9
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-23 13:44:18 +0000

    __operations

--- 01_basics/04_Operations.js
+++ 01_basics/04_Operations.js
@@ -1,6 +1,7 @@
 let value = 3
 let negValue= -value
 
+/*
 console.log(negValue)
 
  console.log("1" + 0)  //  => 10  , first string then treat all string
@@ -14,4 +15,22 @@ console.log(+"") // => 0
  a=1;
  console.log(a++);
  console.log(++a);
- 
+
+*/ 
+
+//------------***-----------
+
+/*
+console.log("2" > 1); //true
+console.log("02" == 2); // truse beacuse comparion make it both numbers
+console.log("4" > 2); // true
+*/
+
+/* 
+The reason is that equality check == and
+ comparison > < >= <= work diffrently
+ Comperison convert null ro a number,
+ treating it as 0.
+ thats why null>=0 is tru and n 
+
+*/
