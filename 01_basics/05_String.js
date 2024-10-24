bdf0d1433d7dce388d851b828f016a7bb25f7090
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-24 19:49:24 +0000

    string In Depth

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -4,4 +4,23 @@
 const name = "Mujtaba"
 const Age = 21
 //  want to concetnent
-console.log(name + age + "value")
+// console.log(name + Age + "value") // but old fashion
+
+// in new fashion use betricks and ${variable_name.(if You want any function it)}
+console.log(`helo my name is ${name} and age is ${Age} `)
+
+const game = new String('Mujtaba')
+
+console.log(game[0]) // M
+console.log(game.__proto__) // return {}
+
+console.log(game.length) // 7
+console.log(game.indexOf('u')) // 1
+console.log(game.charAt(5)) // b
+
+const Temp= " Hi  ";
+console.log(Temp)
+console.log(Temp.trim( ))// skip spaces
+
+const url = "mujtaba_n@gmail.com"
+console.log(url.replace('_',0))
