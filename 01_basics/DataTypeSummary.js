31741a7323e3c5eb80b1f7d9558cceceffc8b3fb
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-24 18:23:29 +0000

    summary of dataTypes

---
+++ 01_basics/DataTypeSummary.js
@@ -0,0 +1,25 @@
+
+/* 
+ two type of data types : PREMETIVE  & Non- PRIMITIVE
+    
+ => Primitive: means when we use then its copy we acces from memory not actual it
+7 types: string ,  Bigint,Number , Boolean , null , Undefiend, Symbol
+
+ => Non-Primitive or Refrecnce Type : when we acces it access by Refrence From memory like:
+        Arrays , Objects   & function
+
+        JAVASCRIPT IS DYNAMIC BCZ initially no need to define type
+         
+*/
+
+const score = 100 //=> Number
+const ScoreValue= 100.3 //=>number
+const isLogedIn - false //=> boolean
+const Outside= null //=> null means empty
+let userEmail; //=> undefiend or let userEmail= undefiend; same
+const id = Symbol("123");       // symbol type both are diffrent
+const Anotherid = Symbol("123"); 
+
+const BigNumber= 32434232323423;
+
+console.table([])
