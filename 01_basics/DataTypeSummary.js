8406060ef0dd36f36bd223c0501b9b20e4d25e7d
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-24 18:41:42 +0000

    DataTypeSummary

--- 01_basics/DataTypeSummary.js
+++ 01_basics/DataTypeSummary.js
@@ -14,12 +14,50 @@
 
 const score = 100 //=> Number
 const ScoreValue= 100.3 //=>number
-const isLogedIn - false //=> boolean
-const Outside= null //=> null means empty
-let userEmail; //=> undefiend or let userEmail= undefiend; same
+const isloggedIN = false //=> boolean
+const Outside= null //=> type: object (null means empty)
+let userEmail; //=> type: undefiend or let userEmail= undefiend; same
 const id = Symbol("123");       // symbol type both are diffrent
 const Anotherid = Symbol("123"); 
 
-const BigNumber= 32434232323423;
+const BigNumber= 324342323238383423n; //if we add in last it is Bigint
+/*
+console.table([
+    {Variable:"Score", Type:typeof(score)},
+    {Variable:"ScoreValue", Type:typeof(ScoreValue)},
+    {Variable:"IsLogedIn", Type:typeof(isloggedIN)},
+    {Variable:"Outside", Type:typeof(Outside)},
+    {Variable:"UserEmail", Type:typeof(userEmail)},
+    {Variable:"Id", Type:typeof(id)},
+    {Variable:"BigNumber", Type:typeof(BigNumber)}
 
-console.table([])
+])
+
+*/
+
+//  REfrence (Non Primitive)
+
+// Array:
+
+const  heroes = ["abc","def","GHUi"]
+
+// object in currly bracketis object
+
+let myobj = {
+    name: "Mujtaba",
+    age: 21,
+}
+// console.log(myobj, typeof(myobj)) //=> Type = object
+
+//FUNCTION
+/* 
+function defination:
+function(){}
+
+but want to store in variable
+its type will be  Object Function
+*/
+const myFun = function(){
+    console.log("Hi i am function",typeof(myFun)) // type: Object Function
+}
+myFun()
