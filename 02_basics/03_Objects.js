03516aa463760c615d964abc8b3d0ad26e5c4510
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-25 13:04:27 +0000

    objects+symbol

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -5,20 +5,40 @@
 
 // object literals
 
+
+//for symbol use this method compulsory
+const mysm= Symbol("Key1")
+
 const jsUser= {
     name: "Mujtaba",
     fullName: "MujtabaNoondani",
     age: 21,
+    [mysm]: "mykey1",
     email: "muj@globalThis.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 // acces object use Object_name._element_name
 console.log(jsUser.email)
+console.log(jsUser["email"])
 
-// but if key is in string initially so useobject__name["object"]
+// but if key is in string initially so compulsory useobject__name["object"]
 console.log(jsUser["fullName"])
 
+//for symbol use []
+console.log(jsUser[mysm])
+
+// for change instant of Object
+jsUser.email="abc@gmail.com"
+
+console.log(jsUser.email)
+
+// if u want lock value use 
+Object.freeze(jsUser)
+
+jsUser.email="def@gmail.com"
+
+console.log(jsUser.email) //value not changed
 
 
 /* it has keys : values
