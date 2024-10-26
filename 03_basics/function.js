25029c088b1726e46f6b5cffcad165b6e8ea4ddb
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-26 06:19:39 +0000

    simple functions

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -8,3 +8,32 @@ console.log("C");
 }
 
 funName()
+funName // refrence
+
+// parameter taking
+
+// function addtwo( num1 ,num2)
+function addtwo( num1=0 ,num2=0) //by default taking 0 value
+{
+    temp= num1+num2
+    // console.log(temp)
+    return temp
+}
+t= addtwo(2,3)
+console.log(addtwo(2,3))
+console.log(t)
+
+function loginUserMsg(username)
+{
+    //for check string valid
+    if(username) 
+     return `${username} just logged in`
+    return "wrong username"
+}
+
+// console.log(loginUserMsg('Mujtaba'))
+// console.log(loginUserMsg()) // => undefined
+
+
+//like in shopping cart we dont know ohow much item add
+// so we dont use ()
