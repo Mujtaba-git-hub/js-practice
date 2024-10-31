61380a9a25bb785c540021f8ec35bd19c1f03cea
Mujtaba-git-hub <mujtabaoffical9@gmail.com>
2024-10-31 09:06:46 +0000

    function updates

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -23,7 +23,7 @@ t= addtwo(2,3)
 console.log(addtwo(2,3))
 console.log(t)
 
-function loginUserMsg(username)
+function loginUserMsg(username="Mujan") //means uservalue minimum mujan not undefiendin any case
 {
     //for check string valid
     if(username) 
@@ -37,3 +37,27 @@ function loginUserMsg(username)
 
 //like in shopping cart we dont know ohow much item add
 // so we dont use ()
+
+//... raste & spread operator
+
+// function claculateCart(...num1)
+// {
+    
+// }
+
+// function claculateCart(VAL1,VAL2,...num1) //MEANS VAL1, AND VAL2 SHOULD PASS ANITHER HAS CHOICE
+// {
+    
+// }
+
+const user = {
+    username:  "mujtaba",
+    price: 199
+}
+
+// for object in fun
+
+function handle(anyobj)
+{
+    console.log(`username: $[anyobj.uername] \n price: '$anyobj.price $[anyobj.price}"`)
+}
